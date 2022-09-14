41a80ac1a4ef1a9d422d3edf9990551a27884dd4
Nadira Maysa Dyandra <nadira.maysa@gmail.com>
2022-09-14 16:22:57 +0700

    add katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+        'nama' : 'Nadira Maysa Dyandra',
+        'NPM' : '2106632232',
+        'katalog' : data_item_katalog,
+        }
+    return render(request, 'katalog.html', context)
